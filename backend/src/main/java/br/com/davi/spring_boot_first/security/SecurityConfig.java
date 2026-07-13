c7153a53a301bf15f8a28a23cc608ae61e1394f3
Davi Fonseca Viana <daviviana2602@gmail.com>
2026-07-12 22:09:37 -0300

    feat: add cors config

--- backend/src/main/java/br/com/davi/spring_boot_first/security/SecurityConfig.java
+++ backend/src/main/java/br/com/davi/spring_boot_first/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package br.com.davi.spring_boot_first.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -32,7 +33,9 @@ public class SecurityConfig {
         
             .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
 
-            .addFilterAfter(rateLimitFilter, JwtAuthenticationFilter.class);
+            .addFilterAfter(rateLimitFilter, JwtAuthenticationFilter.class)
+
+            .cors(Customizer.withDefaults());
 
 
         return http.build();
